200cd2a91154f2108eca0f63f0b9a64f64658e12
Shubham Jain <shujain@splunk.com>
2019-02-13 19:00:02 -0800

    added ci workflows & update push step to run only for develop branch

--- .circleci/build_and_push.sh
+++ .circleci/build_and_push.sh
@@ -2,7 +2,7 @@
 set -e
 aws ecr get-login --region $AWS_REGION --no-include-email | bash
 echo "Building docker image..."
-cp pkg/fluent-plugin-kubernetes-objects-*.gem docker
+cp /tmp/pkg/fluent-plugin-kubernetes-objects-*.gem docker
 echo "Copy latest fluent-plugin-splunk-hec gem from S3"
 aws s3 cp s3://k8s-ci-artifacts/fluent-plugin-splunk-hec-${FLUENT_SPLUNK_HEC_GEM_VERSION}.gem ./docker
 docker build --no-cache -t splunk/fluent-plugin-kubernetes-objects:ci ./docker
