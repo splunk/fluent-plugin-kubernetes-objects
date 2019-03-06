5184e483771faedf7b2c13f81837724c7f3521d9
Chaitanya Phalak <cphalak@splunk.com>
2019-03-05 19:15:13 -0800

    Added release workflows

---
+++ .circleci/build_and_push_to_dockerhub.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+echo "Building docker image..."
+cp /tmp/pkg/fluent-plugin-kubernetes-metrics-*.gem docker
+VERSION=`cat VERSION`
+docker build --build-arg VERSION=$VERSION --no-cache -t splunk/fluent-plugin-kubernetes-objects:ci ./docker
+docker tag splunk/fluent-plugin-kubernetes-objects:ci splunk/${DOCKERHUB_REPO_NAME}:${VERSION}
+echo "Push docker image to splunk dockerhub..."
+docker login --username=$DOCKERHUB_ACCOUNT_ID --password=$DOCKERHUB_ACCOUNT_PASS
+docker push splunk/${DOCKERHUB_REPO_NAME}:${VERSION} | awk 'END{print}'
+echo "Docker image pushed successfully to docker-hub."
