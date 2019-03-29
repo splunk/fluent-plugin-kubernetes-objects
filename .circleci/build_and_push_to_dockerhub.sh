014965575a0a4b5bd6ae874989e33f637c8d0a01
Donald Tregonning <dtregonning@splunk.com>
2019-03-29 12:36:18 -0700

    Release 1.1.1 prep - CI fixes (#23)
    * prep for 1.1.1 release
    
    * modified CI scripts to support release

--- .circleci/build_and_push_to_dockerhub.sh
+++ .circleci/build_and_push_to_dockerhub.sh
@@ -3,7 +3,7 @@ set -e
 echo "Building docker image..."
 cp /tmp/pkg/fluent-plugin-kubernetes-objects-*.gem docker
 VERSION=`cat VERSION`
-docker build --build-arg VERSION=$VERSION --no-cache -t splunk/fluent-plugin-kubernetes-objects:ci ./docker
+docker build --build-arg VERSION=$VERSION FLUENT_SPLUNK_HEC_GEM_VERSION=$FLUENT_SPLUNK_HEC_GEM_VERSION --no-cache -t splunk/fluent-plugin-kubernetes-objects:ci ./docker
 docker tag splunk/fluent-plugin-kubernetes-objects:ci splunk/${DOCKERHUB_REPO_NAME}:${VERSION}
 echo "Push docker image to splunk dockerhub..."
 docker login --username=$DOCKERHUB_ACCOUNT_ID --password=$DOCKERHUB_ACCOUNT_PASS
