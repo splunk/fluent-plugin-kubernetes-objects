598604bf2514103a8bb2c690fa3ecddd18ab3c7d
Chaitanya Phalak <cphalak@splunk.com>
2020-02-05 14:13:42 -0800

    Update dependencies (#46)

--- .circleci/build_and_push_to_dockerhub.sh
+++ .circleci/build_and_push_to_dockerhub.sh
@@ -4,6 +4,9 @@ FLUENTD_HEC_GEM_VERSION=`cat docker/FLUENTD_HEC_GEM_VERSION`
 echo "Building docker image..."
 cp /tmp/pkg/fluent-plugin-kubernetes-objects-*.gem docker
 VERSION=`cat VERSION`
+echo "Copying licenses to be included in the docker image..."
+mkdir licenses
+cp -rp LICENSE licenses/
 docker build --build-arg VERSION=$VERSION --build-arg FLUENT_SPLUNK_HEC_GEM_VERSION=$FLUENTD_HEC_GEM_VERSION --no-cache -t splunk/fluent-plugin-kubernetes-objects:ci ./docker
 docker tag splunk/fluent-plugin-kubernetes-objects:ci splunk/${DOCKERHUB_REPO_NAME}:${VERSION}
 echo "Push docker image to splunk dockerhub..."
