b4b33c01b53285f35c0f174d977571dd13cf6983
Rock Baek <rbaek@splunk.com>
2020-06-23 00:34:22 -0700

    security library update

--- .circleci/build_and_push.sh
+++ .circleci/build_and_push.sh
@@ -5,7 +5,7 @@ echo "Building docker image..."
 cp /tmp/pkg/fluent-plugin-kubernetes-objects-*.gem docker
 echo "Copy latest fluent-plugin-splunk-hec gem from S3"
 VERSION=`cat VERSION`
-docker build --build-arg VERSION=$VERSION --no-cache -t splunk/fluent-plugin-kubernetes-objects:ci ./docker
+docker build --no-cache --pull --build-arg VERSION=$VERSION -t splunk/fluent-plugin-kubernetes-objects:ci ./docker
 docker tag splunk/fluent-plugin-kubernetes-objects:ci $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/k8s-ci-objects:latest
 echo "Push docker image to ecr..."
 docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/k8s-ci-objects:latest | awk 'END{print}'
