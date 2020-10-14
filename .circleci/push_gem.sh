f0cfb5fefea7fc88f8534a094bc4a4ca5b5c8426
Rock Baek <rbaek@splunk.com>
2020-10-13 18:05:09 -0700

    change it to main (#58)

--- .circleci/push_gem.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-sudo apt-get update
-sudo apt-get install -y python-pip libpython-dev > /dev/null 2>&1
-echo "Installing aws cli..."
-sudo pip install awscli > /dev/null 2>&1
-echo "Pushing objects gem to s3..."
-aws s3 cp /tmp/pkg/*.gem s3://k8s-ci-artifacts/
